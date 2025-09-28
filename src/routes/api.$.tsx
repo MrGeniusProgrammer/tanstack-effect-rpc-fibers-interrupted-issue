dc9bc16da84a698919d259182f387b0376ab88b3
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2025-09-28 11:29:28 +0300

    currying handler with the same instance of web handler

--- src/routes/api.$.tsx
+++ src/routes/api.$.tsx
@@ -1,27 +1,30 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { createServerOnlyFn } from '@tanstack/react-start'
 
-const handler = createServerOnlyFn(async () => {
+const buildHandler = createServerOnlyFn(async () => {
   const { AllRoutes } = await import('@/domains/http')
   const HttpLayerRouter = await import('@effect/platform/HttpLayerRouter')
 
   const { handler: webHandler } = HttpLayerRouter.toWebHandler(AllRoutes)
+
   return async ({ request }: { request: Request }) => {
     return await webHandler(request)
   }
 })
 
+const handler = await buildHandler()
+
 export const Route = createFileRoute('/api/$')({
   server: {
     handlers: {
-      GET: await handler(),
-      POST: await handler(),
-      OPTIONS: await handler(),
-      ALL: await handler(),
-      HEAD: await handler(),
-      DELETE: await handler(),
-      PUT: await handler(),
-      PATCH: await handler(),
+      GET: handler,
+      POST: handler,
+      OPTIONS: handler,
+      ALL: handler,
+      HEAD: handler,
+      DELETE: handler,
+      PUT: handler,
+      PATCH: handler,
     },
   },
 })

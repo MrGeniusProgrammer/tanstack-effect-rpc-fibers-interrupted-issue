d71efdb9af555643dfa6991f565e48ddb487dd01
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2025-09-28 11:20:50 +0300

    tanstack handlers might be the issue

--- src/routes/api.$.tsx
+++ src/routes/api.$.tsx
@@ -1,29 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { createServerOnlyFn } from '@tanstack/react-start'
 
-const handler = createServerOnlyFn(
-  () =>
-    async ({ request }: { request: Request }) => {
-      const { AllRoutes } = await import('@/domains/http')
-      const HttpLayerRouter = await import('@effect/platform/HttpLayerRouter')
+const handler = createServerOnlyFn(async () => {
+  const { AllRoutes } = await import('@/domains/http')
+  const HttpLayerRouter = await import('@effect/platform/HttpLayerRouter')
 
-      const { handler: webHandler } = HttpLayerRouter.toWebHandler(AllRoutes)
-
-      return await webHandler(request)
-    },
-)
+  const { handler: webHandler } = HttpLayerRouter.toWebHandler(AllRoutes)
+  return async ({ request }: { request: Request }) => {
+    return await webHandler(request)
+  }
+})
 
 export const Route = createFileRoute('/api/$')({
   server: {
     handlers: {
-      GET: handler(),
-      POST: handler(),
-      OPTIONS: handler(),
-      ALL: handler(),
-      HEAD: handler(),
-      DELETE: handler(),
-      PUT: handler(),
-      PATCH: handler(),
+      GET: await handler(),
+      POST: await handler(),
+      OPTIONS: await handler(),
+      ALL: await handler(),
+      HEAD: await handler(),
+      DELETE: await handler(),
+      PUT: await handler(),
+      PATCH: await handler(),
     },
   },
 })
